5577dab6b012f55cbe366db400b98dd2f34881c6
Kuzmin Boris <kzmnbrs@icloud.com>
2020-10-05 17:16:24 +0000

    fix(init): Reader wasn't querying nsqlookupd on init

--- app/stream.py
+++ app/stream.py
@@ -2,6 +2,7 @@ from asyncio import Queue
 from typing import List
 
 import nsq
+from tornado.ioloop import IOLoop
 
 
 class NSQMessagesStream:
@@ -9,8 +10,13 @@ class NSQMessagesStream:
         self.reader = nsq.Reader(topic,
                                  channel,
                                  message_handler=self._handle_message,
+                                 lookupd_poll_interval=5,
                                  lookupd_http_addresses=nsqlookupd_addresses)
         self.mediator = Queue()
+
+        # Or client will have to wait `lookupd_pool_interval`
+        # seconds after the service start.
+        IOLoop.current().add_callback(self.reader.query_lookupd)
 
     def _handle_message(self, message: nsq.Message):
         self.mediator.put_nowait(message.body)
